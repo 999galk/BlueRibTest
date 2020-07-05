a9e5df6245c957ee2fc316116cfde0f125818af5
Gal Kol <999galk@gmail.com>
2020-07-05 13:39:12 +0300

    blue rib test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './Components/SignIn/SignIn';
+import Flights from './Components/Flights/Flights';
+import NewFlight from './Components/NewFlight/NewFlight';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {}
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={SignIn}/>
+          <Route exact path='/flights' component={Flights}/>
+          <Route exact path='/newFlight' component={NewFlight}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Data.js
@@ -0,0 +1,5 @@
+export let Data = [
+	{ id: 1, from: 'Israel', to: 'Italy', departure:'2020/10/15 10:20', landing:'2020/10/15 10:20', price: '100$'},
+	{ id: 2, from: 'Israel', to: 'LA', departure:'2020/10/15 10:20', landing:'2020/10/15 10:20', price: '200$'},
+	{ id: 3, from: 'Israel', to: 'India', departure:'2020/10/15 10:20', landing:'2020/10/15 10:20', price: '300$' }
+]
