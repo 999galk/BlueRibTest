6c01d18ed508d2d8dbc71064257fabb195573c53
999galk <999galk@gmail.com>
2020-07-29 11:34:55 +0300

    Updates

---
+++ precache-manifest.387fc1733655e759d8aebe4cc6ee70c8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db005ba3abc30825446908358a8d19bd",
+    "url": "/SH-Law/index.html"
+  },
+  {
+    "revision": "8c21decf422dee67592c",
+    "url": "/SH-Law/static/css/2.476f98bf.chunk.css"
+  },
+  {
+    "revision": "1cd7216cd7d4c7cb5e15",
+    "url": "/SH-Law/static/css/main.1627997f.chunk.css"
+  },
+  {
+    "revision": "8c21decf422dee67592c",
+    "url": "/SH-Law/static/js/2.6be2812d.chunk.js"
+  },
+  {
+    "revision": "2f72f2931131c5cbc5393c51e173de16",
+    "url": "/SH-Law/static/js/2.6be2812d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd7216cd7d4c7cb5e15",
+    "url": "/SH-Law/static/js/main.46b34885.chunk.js"
+  },
+  {
+    "revision": "ed014d8e580ec531167a",
+    "url": "/SH-Law/static/js/runtime-main.1e9449e0.js"
+  },
+  {
+    "revision": "6f6f82d9243d8111301f39648fb1d62b",
+    "url": "/SH-Law/static/media/Bebas-Regular.6f6f82d9.ttf"
+  },
+  {
+    "revision": "d3b7c05fa118bd243fb5b121da6ad8cb",
+    "url": "/SH-Law/static/media/IMG_0149.d3b7c05f.JPG"
+  },
+  {
+    "revision": "8d6728289ad75307f02b33e01305a505",
+    "url": "/SH-Law/static/media/LOGO_2 PNG.8d672828.png"
+  },
+  {
+    "revision": "af2eaf645815c8f618dc79bd6072fe97",
+    "url": "/SH-Law/static/media/People-at-Work.af2eaf64.jpg"
+  },
+  {
+    "revision": "44f28ff5ddf738f8add1df097a7cb4c1",
+    "url": "/SH-Law/static/media/arrow_down2.44f28ff5.png"
+  },
+  {
+    "revision": "d65822cec59cd820d2c153771058be83",
+    "url": "/SH-Law/static/media/hitech.d65822ce.png"
+  },
+  {
+    "revision": "73a921b3f9fc587d8e4301ea940b67db",
+    "url": "/SH-Law/static/media/internet.73a921b3.png"
+  },
+  {
+    "revision": "524c4df3c7dfc099b7303ffeafcf69d2",
+    "url": "/SH-Law/static/media/linkedin-icon-32.524c4df3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SH-Law/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ff0446b87aa279667de8fa6fea9b657",
+    "url": "/SH-Law/static/media/mishari.8ff0446b.png"
+  },
+  {
+    "revision": "3f7a4162c6a629f7d5bb155674b36219",
+    "url": "/SH-Law/static/media/origin_reduced.3f7a4162.jpg"
+  },
+  {
+    "revision": "8aa010284c496c941a53e0b4e0008b3f",
+    "url": "/SH-Law/static/media/pin.8aa01028.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/SH-Law/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/SH-Law/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/SH-Law/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/SH-Law/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a18b8bee57af6438e562dca4b774e48e",
+    "url": "/SH-Law/static/media/stock-photo-selective-focus-of-the-stacking-magazine-place-on-table-in-living-room-662452171.a18b8bee.jpg"
+  }
+]);
